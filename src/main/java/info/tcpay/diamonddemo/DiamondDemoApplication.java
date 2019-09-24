62b0fbdb36118bc6ed2c86ede627be6fa8abfa3d
xuqplus <445172495@qq.com>
2019-09-24 16:51:47 +0800

    init

---
+++ src/main/java/info/tcpay/diamonddemo/DiamondDemoApplication.java
@@ -0,0 +1,13 @@
+package info.tcpay.diamonddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiamondDemoApplication.class, args);
+    }
+
+}
